7b765b75aa7d2574efd5eb18b89bd5a8ce07f349
lisknonanika <lisknonanika@gmail.com>
2021-12-30 11:59:49 +0900

    upd

--- src/index.js
+++ src/index.js
@@ -4,25 +4,64 @@ const schema = require('./config/schema.json');
 const randomNumber = require('random-number-csprng');
 const express = require('express');
 const app = express();
-const networkId = '639cb8adbaa66448501c5bb53e25fafbf97736af6a150f8463b3165f1f10a907';
 const port = 3000;
 const PASSWORD = 'password';
+const ATARISU = 10;
+const KIGEN = 1641258000;
+let NETWORK_ID = undefined;
+
+BigInt.prototype.toJSON = function() { return this.toString(); }
+Buffer.prototype.toJSON = function() { return cryptography.bufferToHex(this); }
 
 const createClient = async() => {
     return await apiClient.createWSClient('http://localhost:8080/ws');
+}
+
+const setNetworkId = async() => {
+    if (NETWORK_ID) return;
+    let client = undefined;
+    try {
+        client = await createClient();
+        const node = await client.node.getNodeInfo();
+        NETWORK_ID = node.networkIdentifier;
+    } finally {
+        if (client) await client.disconnect();
+    }
 }
 
 const getPassphrase = async() => {
     return config[await randomNumber(0, config.length - 1)].passphrase;
 }
 
-const sendTransaction = async(client, param, passphrase) => {
+const getTosensha = async(kekka) => {
+    const tosensha = [];
+
+    // 対象者を重複なしで取得
+    let taishoshas = kekka.filter(v => { return v.jikan <= KIGEN }).map(v => { return v.name });
+    taishoshas = Array.from(new Set(taishoshas));
+    
+    if (taishoshas.length === 0) return [];
+    if (taishoshas.length === 1) return [taishoshas[0]];
+    while (tosensha.length < ATARISU) {
+        const taishosha = taishoshas[await randomNumber(0, taishoshas.length - 1)];
+        tosensha.push(taishosha);
+        taishoshas = taishoshas.filter(v => { return v !== taishosha });
+        if (taishoshas.length === 1 && tosensha.length < ATARISU) {
+            tosensha.push(taishoshas[0]);
+            break;
+        }
+        if (tosensha.length === ATARISU) break;
+    }
+    return tosensha;
+}
+
+const sendTransaction = async(client, param, passphrase, schema) => {
     client = await createClient();
     let tx = await client.transaction.create( param, passphrase );
     tx = transactions.signTransaction(
-        schema.pull,
+        schema,
         tx,
-        Buffer.from(networkId, 'hex'),
+        Buffer.from(NETWORK_ID, 'hex'),
         passphrase
     );
     return await client.transaction.send(tx);
@@ -39,12 +78,12 @@ app.get('/api/omikuji/pull', async(req, res) => {
             nonce: 0,
             senderPublicKey: cryptography.getPrivateAndPublicKeyFromPassphrase(passphrase).publicKey,
             asset: {
-                address: req.address || '',
-                name: req.name || '',
+                address: req.query.address || '',
+                name: req.query.name || '',
                 jikan: Math.floor(new Date().getTime() / 1000)
             }
         }
-        const id = await sendTransaction(client, param, passphrase);
+        const id = await sendTransaction(client, param, passphrase, schema.pull);
         res.json({ result: true, id: id});
 
     } catch(err) {
@@ -62,16 +101,12 @@ app.get('/api/omikuji/atari', async(req, res) => {
 
         // おみくじの結果を取得
         const ret = await client.invoke('omikuji:omikujiKekka');
-        const tousensha = [];
-
-        // TODO 当選者取得
-
-
-        const password = req.password;
+        const tosensha = await getTosensha(ret);
+        const password = req.query.password;
 
         // テスト実行の場合はトランザクションは送信しない
         if (!password || password !== PASSWORD) {
-            res.json({ result: true, id: '', tousensha: tousensha});
+            res.json({ result: true, id: '', data: tosensha});
             return;
         }
         
@@ -83,10 +118,10 @@ app.get('/api/omikuji/atari', async(req, res) => {
             fee: BigInt(10000000),
             nonce: 0,
             senderPublicKey: cryptography.getPrivateAndPublicKeyFromPassphrase(passphrase).publicKey,
-            asset: { name: tousensha, atarisu: 10 }
+            asset: { name: tosensha, atarisu: ATARISU }
         }
-        const id = await sendTransaction(client, param, passphrase);
-        res.json({ result: true, id: id, tousensha: tousensha});
+        const id = await sendTransaction(client, param, passphrase, schema.atari);
+        res.json({ result: true, id: id, data: tosensha});
 
 
     } catch(err) {
@@ -99,11 +134,20 @@ app.get('/api/omikuji/atari', async(req, res) => {
 app.get('/api/omikuji/kekka', async(req, res) => {
     let client = undefined;
     try {
-        const address = req.address;
-        const name = req.name;
         client = await createClient();
         const ret = await client.invoke('omikuji:omikujiKekka');
-        res.json({ result: true, data: ret.omikujiKekka});
+        let kekka = ret;
+
+        // filter適用
+        const address = req.query.address;
+        if (address) {
+            kekka = kekka.filter(v => { return v.address === address });
+        }
+        const name = req.query.name;
+        if (name) {
+            kekka = kekka.filter(v => { return v.name === name });
+        }
+        res.json({ result: true, data: kekka});
 
     } catch(err) {
         res.json({ result: false, msg: err.message });
@@ -112,12 +156,12 @@ app.get('/api/omikuji/kekka', async(req, res) => {
     }
 });
 
-app.get('/api/omikuji/tousensha', async(req, res) => {
+app.get('/api/omikuji/tosensha', async(req, res) => {
     let client = undefined;
     try {
         client = await createClient();
         const ret = await client.invoke('omikuji:omikujiAtari');
-        res.json({ result: true, data: ret.omikujiAtari});
+        res.json({ result: true, data: ret});
 
     } catch(err) {
         res.json({ result: false, msg: err.message });
@@ -126,6 +170,49 @@ app.get('/api/omikuji/tousensha', async(req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`api start -> http://localhost:${port}`)
+app.get('/api/omikuji/account', async(req, res) => {
+    let client = undefined;
+    try {
+        client = await createClient();
+        const ret = await client.account.get(cryptography.getAddressFromBase32Address(req.query.address));
+        res.json({ result: true, data: ret});
+
+    } catch(err) {
+        res.json({ result: false, msg: err.message });
+    }finally {
+        if (client) await client.disconnect();
+    }
+});
+
+app.get('/api/omikuji/transaction', async(req, res) => {
+    let client = undefined;
+    try {
+        client = await createClient();
+        const ret = await client.transaction.get(req.query.id);
+        res.json({ result: true, data: ret});
+
+    } catch(err) {
+        res.json({ result: false, msg: err.message });
+    }finally {
+        if (client) await client.disconnect();
+    }
+});
+
+app.get('/api/omikuji/node', async(req, res) => {
+    let client = undefined;
+    try {
+        client = await createClient();
+        const ret = await client.node.getNodeInfo();
+        res.json({ result: true, data: ret});
+
+    } catch(err) {
+        res.json({ result: false, msg: err.message });
+    }finally {
+        if (client) await client.disconnect();
+    }
+});
+
+app.listen(port, async() => {
+    console.log(`api start -> http://localhost:${port}`);
+    await setNetworkId();
 })
